10bbdc6e578a9bff28d80022472a37bd4c171639
Su401 <81376141+Su401@users.noreply.github.com>
2025-06-20 20:56:08 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # loads .env variables
-
-print("Token loaded:", os.getenv('GITHUB_TOKEN'))
-print("Username loaded:", os.getenv('GITHUB_USERNAME'))
